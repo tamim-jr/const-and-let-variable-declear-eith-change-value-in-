ae912eee52e1416a0f2b9986a4f8a76417a205c9
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-08 23:44:11 +0600

    const and var use in js

---
+++ index.js
@@ -0,0 +1,16 @@
+const wife = "my wife";
+console.log(wife);
+
+const ageValue = [15, 45, 84];
+console.log(ageValue);
+ageValue[2] = 12;
+console.log(ageValue);
+ageValue.push(44); 
+console.log(ageValue);
+
+const hero = {name: "sakib Khan", age: 38, phone: 4875958}
+console.log("First Hero info ", hero);
+hero.name = ("Ayman Sadik");
+hero.age = 26;
+hero.phone = 987654;
+console.log("Last Hero info ", hero);
